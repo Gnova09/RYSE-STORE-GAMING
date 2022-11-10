9c2e4bef085ef90df2afe5c14c0f5401e7fe9a22
Leonysvasquez <47530394+Leonysvasquez@users.noreply.github.com>
2022-11-09 21:02:23 -0500

    Add files via upload

---
+++ Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿namespace RYSE.WEB.Models
+{
+    public class Articulos
+    {
+        public int IdArticulos { get; set; }
+        public string? NameArticulos { get; set; }
+        
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+
+        public string? Color { get; set; }
+
+        public int Tamaño { get; set; }
+
+        public string? Precio { get; set;}
+
+        public int? Peso { get; set; }
+
+    }
+}
